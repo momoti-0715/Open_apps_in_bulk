87ff68dd3247bbf2383e1de7ae7588b1668a6841
momoti-0715 <ayapon2003@outlook.jp>
2026-04-18 22:32:17 +0900

    コンポーネントを配置

--- Open_apps_in_bulk/Form1.Designer.cs
+++ Open_apps_in_bulk/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.shortcutList = new System.Windows.Forms.ListBox();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.Font = new System.Drawing.Font("Noto Sans JP", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonNew.Location = new System.Drawing.Point(633, 33);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(148, 49);
+            this.buttonNew.TabIndex = 0;
+            this.buttonNew.Text = "新規";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            // 
+            // shortcutList
+            // 
+            this.shortcutList.FormattingEnabled = true;
+            this.shortcutList.ItemHeight = 15;
+            this.shortcutList.Location = new System.Drawing.Point(47, 33);
+            this.shortcutList.Name = "shortcutList";
+            this.shortcutList.Size = new System.Drawing.Size(546, 469);
+            this.shortcutList.TabIndex = 3;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Font = new System.Drawing.Font("Noto Sans JP", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonEdit.Location = new System.Drawing.Point(633, 115);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(148, 49);
+            this.buttonEdit.TabIndex = 4;
+            this.buttonEdit.Text = "編集";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Font = new System.Drawing.Font("Noto Sans JP", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonDel.Location = new System.Drawing.Point(633, 453);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(148, 49);
+            this.buttonDel.TabIndex = 5;
+            this.buttonDel.Text = "削除";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(817, 544);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.shortcutList);
+            this.Controls.Add(this.buttonNew);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.ListBox shortcutList;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDel;
     }
 }
 
