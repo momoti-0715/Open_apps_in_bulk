64127746822ceb7781e71e2f14cc3a39a9e9bd72
momoti-0715 <ayapon2003@outlook.jp>
2026-04-20 03:32:28 +0900

    変更画面を追加

---
+++ Open_apps_in_bulk/Form3.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Open_apps_in_bulk
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCansel = new System.Windows.Forms.Button();
+            this.userControl11 = new Open_apps_in_bulk.UserControl1();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonSave);
+            this.panel1.Controls.Add(this.buttonCansel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 702);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1043, 62);
+            this.panel1.TabIndex = 14;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(762, 10);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(130, 41);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "保存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonCansel
+            // 
+            this.buttonCansel.Location = new System.Drawing.Point(901, 10);
+            this.buttonCansel.Name = "buttonCansel";
+            this.buttonCansel.Size = new System.Drawing.Size(130, 41);
+            this.buttonCansel.TabIndex = 3;
+            this.buttonCansel.Text = "キャンセル";
+            this.buttonCansel.UseVisualStyleBackColor = true;
+            // 
+            // userControl11
+            // 
+            this.userControl11.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userControl11.AutoScroll = true;
+            this.userControl11.Location = new System.Drawing.Point(6, -1);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(1031, 697);
+            this.userControl11.TabIndex = 15;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 764);
+            this.Controls.Add(this.userControl11);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form3";
+            this.Text = "編集";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCansel;
+        private UserControl1 userControl11;
+    }
+}
