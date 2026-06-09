84b02e7ae542716d2ee3b0ffcfa3a4919fbf21bf
momoti-0715 <ayapon2003@outlook.jp>
2026-06-09 23:04:09 +0900

    fix

--- Open_apps_in_bulk/ControlJson.cs
+++ Open_apps_in_bulk/ControlJson.cs
@@ -86,6 +86,15 @@ public class ControlJson
                 // サブアイテムの取得
                 string subValue = item.SubItems[1].Text;
 
+                if (string.IsNullOrWhiteSpace(mainValue))
+                {
+                    mainValue = null;
+                }
+                if (string.IsNullOrWhiteSpace(subValue))
+                {
+                    subValue = null;
+                }
+
                 setting.Cmd_open.P_c_list.Add(new PCList
                 {
                     Path = mainValue,
